0f1cf082cf633d30ea003caa3ecad610832caefe
Regcent <vincent.regnard.de.lagny@gmail.com>
2020-12-23 11:28:08 +0100

    Part1 works! Let's go for Part2, probably way more efficient approach

--- Day23/aoc_23.py
+++ Day23/aoc_23.py
@@ -27,17 +27,22 @@ def your_script(raw_data: str) -> Union[int, str, float, bool]:
     Time to code! Write your code here to solve today's problem
     """
     cups = initialize_cups(9, raw_data, True)
-    pretty_print_cups(cups, 10)
     current_cup_label = int(raw_data[0])
-    perform_game(cups, current_cup_label, 1)
+    perform_game(cups, current_cup_label, 100)
+    print(f"Result Part 1 : {prepare_result_1(cups)}")
 
 def perform_game(cups: list, current_cup_label: int, turns: int) -> None:
+    max_label = len(cups)
     for i in range(turns):
-        print(f"\nRound {i}")
-        pretty_print_cups(cups, 10)
-        print(f"Current cup : {current_cup_label}")
+        #print(f"\nRound {i+1}")
+        #pretty_print_cups(cups, 10)
+        #print(f"Current cup : {current_cup_label}")
         selected_cups_labels = select_cups(cups, current_cup_label)
-        print(f"Selected : {selected_cups_labels}")
+        #print(f"Selected : {selected_cups_labels}")
+        destination_label = choose_destination(current_cup_label, selected_cups_labels, max_label)
+        #print(f"Destination : {destination_label}")
+        perform_transformation(cups, current_cup_label, selected_cups_labels, destination_label)
+        current_cup_label = get_labeled_cup(cups, current_cup_label).next
 
 def select_cups(cups: list, current_cup_label: int) -> list:
     selected_cups_labels = []
@@ -46,6 +51,27 @@ def select_cups(cups: list, current_cup_label: int) -> list:
         current_cup = get_labeled_cup(cups, current_cup.next)
         selected_cups_labels.append(current_cup.label)
     return selected_cups_labels
+
+def choose_destination(current_cup_label: int, selected_cups_labels: list, max_label: int) -> int:
+    destination_label = current_cup_label - 1
+    while destination_label in selected_cups_labels or destination_label <= 0:
+        destination_label -= 1
+        if destination_label <= 0:
+            destination_label = max_label
+    return destination_label
+
+def perform_transformation(cups: list, current_cup_label: int, selected_cups_labels: list, destination_label: int) -> None:
+    #Remove the selected cups
+    last_selected_cup = get_labeled_cup(cups, selected_cups_labels[-1])
+    get_labeled_cup(cups, current_cup_label).next = last_selected_cup.next
+    get_labeled_cup(cups, last_selected_cup.next).previous = current_cup_label
+    #Insert the selected cups
+    destination_cup = get_labeled_cup(cups, destination_label)
+    destination_neighbor_label = destination_cup.next
+    destination_cup.next = selected_cups_labels[0]
+    get_labeled_cup(cups, selected_cups_labels[0]).previous = destination_label
+    last_selected_cup.next = destination_neighbor_label
+    get_labeled_cup(cups, destination_neighbor_label).previous = selected_cups_labels[-1]
 
 def initialize_cups(count: int, raw_data: str, part1: bool) -> list:
     cups = [Cup(i, 1, count) for i in range(1, count + 1)]
@@ -69,7 +95,7 @@ def initialize_cups(count: int, raw_data: str, part1: bool) -> list:
         last_cup.next = len(raw_data)
     return cups
 
-def pretty_print_cups(cups: int, count: int) -> None:
+def pretty_print_cups(cups: list, count: int) -> None:
     labels = [str(cups[0].label)]
     current_cup = get_labeled_cup(cups, cups[0].next)
     i = 1
@@ -79,8 +105,16 @@ def pretty_print_cups(cups: int, count: int) -> None:
         i += 1
     print("Cups : " + ", ".join(labels))
 
+def prepare_result_1(cups: list) -> str:
+    labels = []
+    current_cup = get_labeled_cup(cups, cups[0].next)
+    while not current_cup == cups[0]:
+        labels.append(str(current_cup.label))
+        current_cup = get_labeled_cup(cups, current_cup.next)
+    return "".join(labels)
+    
 def get_labeled_cup(cups: list, label: int) -> Cup:
     return cups[label - 1]
 
 if __name__ == "__main__":
-    print(run_script("input.txt"))
+    print(run_script("example.txt"))
